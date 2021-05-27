972b593d557b98dbb70f1ffb0b1bb1955da3f1c2
Yura Luttsev <y.luttsev@gmail.com>
2021-05-27 15:12:52 +0600

    ver 1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// 'use strict'
+// let forms=document.querySelector('#forms');
+// let btn=document.querySelector('.contacts_btn');
+// let answer=document.querySelector('.answer');
+// let btnForm=document.querySelector('#btn');
+//     btn.addEventListener('click', function(){
+//         forms.style.display='block'
+//     });
+//     btnForm.addEventListener('click', function(){
+//         forms.style.display='none',
+//         answer.style.display='block'
+//     });
+
